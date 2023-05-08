0d8f68e7b966005e1f7e1649858575e1e11d4f0e
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-08 05:45:11 -0600

    fixed associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   def create
-    new_post = Post.create(content: params[:content])
+    new_post = Post.create(content: params[:content], user_id: params[:user_id])
 
     render json: { post: new_post}
   end
