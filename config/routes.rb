1fe57475535f796410fa648b68c274f37912397b
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-08 04:59:52 -0600

    have post and user tables, issue with association

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'static_pages#index'
+  post '/posts', to: 'posts#create'
+  get '/posts', to: 'posts#index'
+  post '/users', to: 'users#create'
+  get '/users', to: 'users#index'
 end
